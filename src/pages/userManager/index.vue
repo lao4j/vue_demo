ea6506f878be1d5b87938e1c28ee540d2f5070f2
mtvcyx@hotmail.com <mtvcyx@hotmail.com>
2018-09-16 19:33:20 +0800

    first

---
+++ src/pages/userManager/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        This is user manager.
+    </div>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+      testData: 'this a test'
+    }
+  }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/userManager',
+      name: 'userManager',
+      meta: {
+        title: '用户管理'
+      },
+      component: () => import('@/pages/userManager/index.vue')
+    }
+  ]
+})
