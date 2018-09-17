443faced6afd54a2e9b05cec0ca1dd8ee85fb576
mtvcyx@hotmail.com <mtvcyx@hotmail.com>
2018-09-17 22:11:37 +0800

    update

--- src/pages/userManager/userManager.vue
+++ src/pages/userManager/userManager.vue
@@ -1,23 +1,80 @@
 <template>
-    <Form ref="formInline" :model="formInline" :rules="ruleInline" inline>
-        <FormItem prop="user">
-            <Input type="text" v-model="formInline.user" placeholder="Username">
-                <Icon type="ios-person-outline" slot="prepend"></Icon>
-            </Input>
-        </FormItem>
-        <FormItem prop="password">
-            <Input type="password" v-model="formInline.password" placeholder="Password">
-                <Icon type="ios-locked-outline" slot="prepend"></Icon>
-            </Input>
-        </FormItem>
-        <FormItem>
-            <Button type="primary" @click="submitAction('formInline')">登录</Button>
-        </FormItem>
-    </Form>
+    <div class="layout">
+        <Layout>
+            <Header>
+                <Menu mode="horizontal" theme="dark" active-name="1">
+                    <div class="layout-logo"></div>
+                    <div class="layout-nav">
+                        <MenuItem name="1">
+                            <Icon type="ios-navigate"></Icon>
+                            Item 1
+                        </MenuItem>
+                        <MenuItem name="2">
+                            <Icon type="ios-keypad"></Icon>
+                            Item 2
+                        </MenuItem>
+                        <MenuItem name="3">
+                            <Icon type="ios-analytics"></Icon>
+                            Item 3
+                        </MenuItem>
+                        <MenuItem name="4">
+                            <Icon type="ios-paper"></Icon>
+                            Item 4
+                        </MenuItem>
+                    </div>
+                </Menu>
+            </Header>
+            <Layout :style="{padding: '0 50px'}">
+                <Breadcrumb :style="{margin: '16px 0'}">
+                    <BreadcrumbItem>Home</BreadcrumbItem>
+                    <BreadcrumbItem>Components</BreadcrumbItem>
+                    <BreadcrumbItem>Layout</BreadcrumbItem>
+                </Breadcrumb>
+                <Content :style="{padding: '24px 0', minHeight: '280px', background: '#fff'}">
+                    <Layout>
+                        <Sider hide-trigger :style="{background: '#fff'}">
+                            <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
+                                <Submenu name="1">
+                                    <template slot="title">
+                                        <Icon type="ios-navigate"></Icon>
+                                        Item 1
+                                    </template>
+                                    <MenuItem name="1-1">Option 1</MenuItem>
+                                    <MenuItem name="1-2">Option 2</MenuItem>
+                                    <MenuItem name="1-3">Option 3</MenuItem>
+                                </Submenu>
+                                <Submenu name="2">
+                                    <template slot="title">
+                                        <Icon type="ios-keypad"></Icon>
+                                        Item 2
+                                    </template>
+                                    <MenuItem name="2-1">Option 1</MenuItem>
+                                    <MenuItem name="2-2">Option 2</MenuItem>
+                                </Submenu>
+                                <Submenu name="3">
+                                    <template slot="title">
+                                        <Icon type="ios-analytics"></Icon>
+                                        Item 3
+                                    </template>
+                                    <MenuItem name="3-1">Option 1</MenuItem>
+                                    <MenuItem name="3-2">Option 2</MenuItem>
+                                </Submenu>
+                            </Menu>
+                        </Sider>
+                        <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
+                            Content
+                        </Content>
+                    </Layout>
+                </Content>
+            </Layout>
+            <Footer class="layout-footer-center">2011-2016 &copy; TalkingData</Footer>
+        </Layout>
+    </div>
+</div>
 </template>
 
-<style>
-@import url('userManager.css');
+<style lang='css' scoped>
+@import 'userManager.css';
 </style>
 
 <script>
@@ -30,19 +87,20 @@ export default {
         password: ''
       },
       ruleInline: {
-        user: [
-          { required: true, message: '请填写用户名', trigger: 'blur' }
-        ],
+        user: [{ required: true, message: '请填写用户名', trigger: 'blur' }],
         password: [
           { required: true, message: '请填写密码', trigger: 'blur' },
-          { type: 'string', min: 6, message: '密码长度不能小于6位', trigger: 'blur' }
+          {
+            type: 'string',
+            min: 6,
+            message: '密码长度不能小于6位',
+            trigger: 'blur'
+          }
         ]
       }
     }
   },
-  computed: {
-
-  },
+  computed: {},
   methods: {
     submitAction: function (e) {
       debugger
